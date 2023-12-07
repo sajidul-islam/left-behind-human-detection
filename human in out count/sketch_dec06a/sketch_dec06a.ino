c1eb8ecc59b350f8c3cdb2021c0d9d8ec3a8e056
sajidul-islam <36081755+sajidul-islam@users.noreply.github.com>
2023-12-07 10:46:25 +0300

    Update sketch_dec06a.ino
    In out count detection

--- human in out count/sketch_dec06a/sketch_dec06a.ino
+++ human in out count/sketch_dec06a/sketch_dec06a.ino
@@ -1,14 +1,41 @@
+const int sensor1Pin = 2;   
+const int sensor2Pin = 3;   
+int count = 0;              
+unsigned long sensorTime = 0;  
+
+bool sensor1crossed = false;
+bool sensor2crossed = false;
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  Serial.begin(9600);
+  pinMode(sensor1Pin, INPUT_PULLUP);
+  pinMode(sensor2Pin, INPUT_PULLUP);
+  Serial.begin(9600); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(digitalRead(2));
-  Serial.println(digitalRead(3));
+  
+  walkin();
   
 
+}
+
+void walkin()
+{
+  int sensor1State = digitalRead(sensor1Pin);
+  int sensor2State = digitalRead(sensor2Pin);
+
+  if (sensor1State == LOW && sensor1crossed == false) 
+  {
+    sensor1crossed =true;
+    sensorTime = millis();
+    Serial.println(sensorTime);
+    Serial.println("Sensor1 crossed");
+  }
+  if (sensor2State == LOW && millis()- sensorTime<=1000)
+  {
+    count++;
+    Serial.println(count);	
+    delay(50);
+  }
+  sensor1crossed = false;
 }
